671bdc65a2211ccd956043b014972918787d413f
Beggie98 <bjuraev2020@gmail.com>
2023-04-18 13:07:01 +0500

    Update DAY1.sql

--- DAY1.sql
+++ DAY1.sql
@@ -177,6 +177,7 @@ WHERE EMPLOYEE_ID = 110
                 OR EMPLOYEE_ID = 116;
                 
 -- above query can be written much simpler way using IN (VALUE1, VALUE2, VALUE3,....)
+--IN operator allows you to check for multiple values of same column
 SELECT * FROM EMPLOYEES
 WHERE EMPLOYEE_ID IN (110, 112, 115, 116);
 
@@ -186,8 +187,136 @@ SELECT FIRST_NAME , JOB_ID
 FROM EMPLOYEES
 WHERE JOB_ID IN ( 'AD_ASST', 'FI_MGR', 'FI_ACCOUNT');
 
+-- display all Department information from Department table
+-- if the DEPARTMENT_NAME s are as below
+-- IT , Public Relations , Sales , Executive
+
+SELECT * FROM DEPARTMENTS
+WHERE DEPARTMENT_NAME IN ('IT', 'Public Relations' , 'Sales' , 'Executive');
+
+-- NULL VALUE? How to define condition for null value
+-- diplay all employees firstname and commission_pct
+-- if the commission_pct value is NULL
+--NULL  is a keyword in SQL to define a cell with no value
+-- in order to say a column value IS NULL we use keyword IS NULL (not = NULL)
+
+SELECT FIRST_NAME , COMMISSION_PCT
+FROM EMPLOYEES
+WHERE COMMISSION_PCT IS NULL;
+
+-- display all employees FIRST_NAME and COMMISSION_PCT
+-- if the COMMISSION_PCT value IS NOT NULL
+
+SELECT FIRST_NAME , COMMISSION_PCT
+FROM EMPLOYEES
+WHERE COMMISSION_PCT IS NOT NULL;
+
+-- display all employees FIRST_NAME and COMMISSION_PCT
+-- if the COMMISSION_PCT value IS NOT NULL
+-- and COMMISSION_PCT is greater than 0.2
+
+SELECT FIRST_NAME , COMMISSION_PCT
+FROM EMPLOYEES
+WHERE COMMISSION_PCT IS NOT NULL 
+AND COMMISSION_PCT > 0.2;
+
+-- How to sort or order the result by certain column name or column index
+-- we can use ORDER BY COLUMN_NAME or ORDER BY COLUMN_INDEX
+-- If not specified, it will by default order by low-to-high order
+-- You can explicitly specify the order using ASC for low-to-high and DESC for high-to-low
+--ORDER BY should appear at the last line
+
+-- sort the result by LAST_NAME column 
+SELECT * FROM EMPLOYEES
+ORDER BY LAST_NAME;         --ORDERS IN ALPHABETICAL ORDER
+
+-- sort the result by FIRST_NAME column in ASCENDING ORDER (low-to-high) explicitly
+SELECT * FROM EMPLOYEES
+ORDER BY FIRST_NAME ASC;
 
 
-                
+-- sort the result by FIRST_NAME column in DESCENDING ORDER (high-to-low) explicitly
+SELECT * FROM EMPLOYEES
+ORDER BY FIRST_NAME DESC;
 
+-- sort the result by SALARY column in DESCENDING ORDER (high-to-low) explicitly
+SELECT * FROM EMPLOYEES
+ORDER BY SALARY DESC;
+
+-- display everything from employees table
+-- if the department_id is 80
+-- sort the result by SALARY column in DESCENDING ORDER (high-to-low) explicitly
+SELECT * FROM EMPLOYEES
+WHERE DEPARTMENT_ID = 80
+ORDER BY SALARY DESC;
+
+-- ORDER BY COLUMN_INDEX
+SELECT * FROM EMPLOYEES
+ORDER BY 2 ASC; -- ORDER BY 2ND COLUMN (FIRST_NAME BECAUSE IT IS IN SECOND COLUMN)
+
+-- ORDER THE RESULT BY FIRST COLUMN DESC
+SELECT * FROM EMPLOYEES
+ORDER BY 1 DESC; -- ORDER BY 1ST COLUMN (EMPLOYEE_ID)
+
+-- USE INDEX TO SORT BY SALARY COLUMN ASC ORDER
+SELECT * FROM EMPLOYEES
+ORDER BY 8;-- SALARY
+
+-- USING PARTIAL SEARCH WITH WILDCARD : LIKE %
+-- LIKE is used with % (0 or more character) to define partial logic in condition
+-- for example
+-- FIRST_NAME LIKE 'A%' means any first_name that starts with letter A and followed by 0 or more characters
+-- FIND OUT ALL EMPLOYEES FIRST_NAME START WITH A
+SELECT FIRST_NAME 
+FROM EMPLOYEES
+WHERE FIRST_NAME LIKE 'A%';
+
+--FIND OUT ALL EMPLOMEES FIRST_NAMES END WITH 'a' ---> '%a'
+SELECT FIRST_NAME 
+FROM EMPLOYEES
+WHERE FIRST_NAME LIKE '%a';
+
+--FIND OUT ALL EMPLOMEES FIRST_NAMES STARTS WIH 'L' END WITH 'a' ---> 'L%a'
+SELECT FIRST_NAME 
+FROM EMPLOYEES
+WHERE FIRST_NAME LIKE 'L%a';
+
+--FIND OUT ALL employees FIRST_NAMES contains 'v'  ---> '%v%'
+SELECT FIRST_NAME
+FROM EMPLOYEES
+WHERE FIRST_NAME LIKE '%v%';
+
+--Find out all phone numbers that start with 650
+
+SELECT FIRST_NAME , PHONE_NUMBER
+FROM EMPLOYEES
+WHERE PHONE_NUMBER LIKE '650%';
+
+-- Find out all JOB_ID that ends with clerk employees table
+SELECT DISTINCT JOB_ID
+FROM EMPLOYEES
+WHERE  JOB_ID LIKE '%CLERK';
+
+--Find all phone numbers that contain 44
+SELECT PHONE_NUMBER
+FROM EMPLOYEES
+WHERE PHONE_NUMBER LIKE '%44%';
+
+-- % can represent 0 or more characters
+-- _ (underscore) can represent exactly one character
+
+--FIND OUT EMPLOYEES NAME START WITH T
+SELECT FIRST_NAME 
+FROM EMPLOYEES
+WHERE FIRST_NAME LIKE 'T%';
+
+--FIND OUT EMPLOYEES NAME START WITH T AND FOLLOWED BY SINGLE CHARACTER (IT MUST HAVE 2 CHARACTER ONLY)
+SELECT FIRST_NAME 
+FROM EMPLOYEES
+WHERE FIRST_NAME LIKE 'T_';
+
+-- find out employee FIRST_NAME start with 'A' and have exactly 4 characters total ---> 'A___' (3 underscores)
+SELECT FIRST_NAME
+FROM EMPLOYEES
+WHERE FIRST_NAME LIKE 'A___';
 
