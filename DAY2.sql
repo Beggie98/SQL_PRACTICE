9a268dd391d61c3b68af8e10a185fb13f04fb3b4
Beggie98 <bjuraev2020@gmail.com>
2023-04-18 18:11:02 +0500

    Update DAY2.sql

--- DAY2.sql
+++ DAY2.sql
@@ -50,3 +50,79 @@ WHERE COUNTRY_ID IN ('UK' , 'US');
 SELECT * FROM COUNTRIES
 WHERE REGION_ID = 1 AND COUNTRY_NAME != 'Belgium';
 
+
+--- CONCATENATION IN SQL  using  || (we do not use + in SQL for concatenation)
+
+-- you can combine String with anything using || 
+-- for example COLUMN1 || COLUMN2    OR   'String here' || 'Another String Here'
+
+-- Find full names of all employees
+SELECT FIRST_NAME ||  '  ' ||    LAST_NAME AS FULL_NAME
+FROM EMPLOYEES;
+
+-- display the result as below
+-- First_Name Makes XXX Money
+
+SELECT FIRST_NAME || ' makes $' || SALARY  AS MONEY
+FROM EMPLOYEES;
+
+-- ARITHMETIC OPERATIONS: +  -  *  /
+
+
+-- Find First_name  , Salary and Salary after $5000 raise from Employees table
+SELECT FIRST_NAME , SALARY , SALARY + 5000 AS AFTER_RAISE
+FROM EMPLOYEES;
+
+-- Find First_name , Salary and Salary cut by 2000
+SELECT FIRST_NAME , SALARY , SALARY - 2000 AS AFTER_CUT
+FROM EMPLOYEES;
+
+--Find Yearly Salary of all employees
+-- If you want to have more than one word in alias you can use " double quote (ONLY FOR ALIAS)
+SELECT FIRST_NAME , SALARY , SALARY * 12 AS "YEARLY SALARY" --- YEARLY_SALARY
+FROM EMPLOYEES;
+
+
+
+
+-- Functions in SQL -- Pre-written functionality we can directly use
+
+-- Single Row Functions - gives the results for each and every row
+
+--Here are a few String related functions in SQL
+-- UPPER(value)            -        makes value upperCase
+-- LOWER(value)          -        makes value lowerCase
+-- LENGTH(value)         -        get the count or size of characters
+
+--Get the first_name of Employee table in all caps (UPPERCASE)
+SELECT FIRST_NAME , UPPER(FIRST_NAME) ,  LOWER(FIRST_NAME) , LENGTH(FIRST_NAME)
+FROM EMPLOYEES;
+
+--Find the employee names with exactly 6 characters
+SELECT FIRST_NAME 
+FROM EMPLOYEES
+WHERE LENGTH(FIRST_NAME) = 6;
+
+-- Find any employee name that has letter 'a' in it, CASE INSENSITIVE
+SELECT FIRST_NAME
+FROM EMPLOYEES
+WHERE LOWER(FIRST_NAME) LIKE '%a%';    
+--WHERE FIRST_NAME LIKE '%A%' OR FIRST_NAME LIKE '%a%' - ALTERNATIVE SOLUTION
+--WHERE UPPER(FIRST_NAME) LIKE '%A%' - ONE MORE ALTERNATIVE SOLUTION
+
+
+-- Number related Single row function: Round(number) OR Round(Number , digitCountYouWantToKeep)
+
+--Find First_Name, Salary,  Daily salary of each employee from Employees table
+SELECT FIRST_NAME , 
+                SALARY , 
+                SALARY/30 , 
+                ROUND(SALARY/30) AS "DAILY SALARY",
+                ROUND(SALARY/30 , 3) --- OVERLOADED FUNCTION: Number 3 defines how many decimal points you want to keep
+FROM EMPLOYEES;
+
+
+
+-- Multi Row Functions
+
+
